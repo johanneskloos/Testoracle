0e5bcb9e4198a2e188fd5d9a7b2251b7682a3d77
Johannes Kloos <jkloos@instartlogic.com>
2015-07-16 10:14:08 -0700

    Better error logging.

--- pprichtrace.ml
+++ pprichtrace.ml
@@ -1,8 +1,11 @@
 open Trace;;
 open Richtrace;;
-let (|>) x f = f x in
-  Array.get Sys.argv 1
+open Printexc;;
+
+record_backtrace true;;
+
+Array.get Sys.argv 1
     |> open_in
     |> parse_tracefile
     |> calculate_rich_tracefile
-    |> Format.printf "%a@." pp_rich_tracefile
+    |> print (Format.printf "%a@." pp_rich_tracefile)
