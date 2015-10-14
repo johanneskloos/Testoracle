3a38ddc8710e3c3fb1e79be31d81fd04a76113be
Johannes Kloos <jkloos@instartlogic.com>
2015-10-14 16:45:34 -0700

    Added base data for testing.

---
+++ test_base_data.ml
@@ -0,0 +1,26 @@
+(** Basic data for tests. *)
+open Types
+
+(** Common instances of JavaScript values - base instances. *)
+let vundef = OUndefiend
+let vnull = ONull
+let vtrue = OBoolean true
+let vfalse = OBoolean false
+let v0 = ONumberInt 0
+let v1 = ONumberInt 1
+let v2 = ONumberInt 2
+let vpi = ONumberFloat 3.14
+let vsqrt2 = ONumberInt 1.41
+let valpha = OString "alpha"
+let vbeta = OString "beta"
+let vmain = OSymbol "main"
+
+(** Three versions of function tables, two equivalent but permuted, and one different. *)
+let funcin1 = Local { from_toString = "func 1 toString"; from_jalangi = Some "func 1 jalangi" }
+let funcin2 = Local { from_toString = "func 2 toString"; from_jalangi = Some "func 2 jalangi" }
+let funcin3 = Local { from_toString = "func 3 toString"; from_jalangi = Some "func 3 jalangi" }
+let funcstd = Local { from_toString = "func std toString"; from_jalangi = None }
+let funcext1 = External 1
+let funcext2 = External 2
+
+ 
