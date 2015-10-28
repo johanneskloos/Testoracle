0360640d01c0234d7ed32540d79db4e9fb1d93b9
Johannes Kloos <jkloos@instartlogic.com>
2015-10-28 14:41:48 -0700

    Clean up interface.

--- calculateVersions.mli
+++ calculateVersions.mli
@@ -1,7 +1,3 @@
-open LocalFacts
-open Reference
-open Types
-
 (** This function fills in the [aliases] and [version] fields of the given
  * trace file. Note that this requires the arguments facts to be filled in. *)
-val calculate_versions: arguments_tracefile -> facts_tracefile
+val calculate_versions: LocalFacts.arguments_tracefile -> LocalFacts.facts_tracefile
