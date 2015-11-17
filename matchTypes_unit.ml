a0c66bbaef5cfbbd069b2c887831a0d193538793
jkloos <jkloos@mpi-sws.org>
2015-11-17 19:23:33 +0100

    Add test cases for MatchTypes.

---
+++ matchTypes_unit.ml
@@ -0,0 +1,34 @@
+open Kaputt.Abbreviations
+open MatchTypes
+
+let pp_match_state pp = let open Format in function
+	| InToplevel -> pp_print_string pp "InToplevel"
+	| InRegular -> pp_print_string pp "InRegular"
+	| InRegularEnter -> pp_print_string pp "InRegularEnter"
+	| InWrap -> pp_print_string pp "InWrap"
+	| InToString -> pp_print_string pp "InToString"
+	| InExternal -> pp_print_string pp "InExternal"
+	| InInit -> pp_print_string pp "InInit"
+	| InWrapperEnter -> pp_print_string pp "InWrapperEnter"
+	| InIndirectDefinitionPattern -> pp_print_string pp "InIndirectDefinitionPattern"
+	| InExtraFunctionPattern -> pp_print_string pp "InExtraFunctionPattern"
+	| InToStringUpdatePattern -> pp_print_string pp "InToStringUpdatePattern"
+
+let test_get_state_inner (name, stack, state) =
+	Test.make_simple_test ~title:name (fun () ->
+		Assert.make_equal (=) (Misc.to_string pp_match_state) (get_state stack) state)
+
+let _ =
+	Test.run_tests (List.map test_get_state_inner [
+		("get_state - InToplevel", [], InToplevel);
+		("get_state - InRegular", [Regular], InRegular);
+		("get_state - InRegularEnter", [RegularEnter], InRegularEnter);
+		("get_state - InWrap", [Wrapper], InWrap);
+		("get_state - InToString", [ToString], InToString);
+		("get_state - InExternal", [External], InExternal);
+		("get_state - InInit", [Init], InInit);
+		("get_state - InWrapperEnter", [WrapperEnter], InWrapperEnter);
+		("get_state - InIndirectDefinitionPattern", [IndirectDefinitionPattern], InIndirectDefinitionPattern);
+		("get_state - InExtraFunctionPattern", [ExtraFunctionPattern], InExtraFunctionPattern);
+		("get_state - InToStringUpdatePattern", [ToStringUpdatePattern], InToStringUpdatePattern);
+		])
