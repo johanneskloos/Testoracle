b07238f2c8069a75f51d8a02d3c5f8c6b1a39b08
jkloos <jkloos@mpi-sws.org>
2016-01-11 14:41:22 +0100

    Remove files that have been extracted to js-enrichment.

--- calculatePointsTo.mli
+++
@@ -1,6 +0,0 @@
-open LocalFacts
-open PointsTo
-
-(** Calculate a points-to map for a given trace file. Note that this requires
- * the version map to be initialized. *)
-val calculate_pointsto : facts_tracefile -> points_to_map
--- calculateVersions.mli
+++
@@ -1,3 +0,0 @@
-(** This function fills in the [aliases] and [version] fields of the given
- * trace file. Note that this requires the arguments facts to be filled in. *)
-val calculate_versions: LocalFacts.arguments_tracefile -> LocalFacts.facts_tracefile
